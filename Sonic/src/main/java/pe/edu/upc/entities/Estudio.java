6a5a3773052c4e84f0af0027ed9cd8e9b1978344
Michael Alfaro <u201618332@upc.edu.pe>
2022-05-02 00:54:37 -0500

    Registro de Salas
    Registro de Salas con combobox

--- Sonic/src/main/java/pe/edu/upc/entities/Estudio.java
+++ Sonic/src/main/java/pe/edu/upc/entities/Estudio.java
@@ -1,5 +1,7 @@
 package pe.edu.upc.entities;
 
+import java.util.Objects;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -94,4 +96,23 @@ public class Estudio {
 		NCalificacion = nCalificacion;
 	}
 
+	@Override
+	public int hashCode() {
+		return Objects.hash(CEstudio);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Estudio other = (Estudio) obj;
+		return CEstudio == other.CEstudio;
+	}
+
+	
+	
 }
