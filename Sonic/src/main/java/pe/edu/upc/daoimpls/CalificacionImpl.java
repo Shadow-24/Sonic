8abf5d43dac5340feb25321f678b8b3f355ba5bc
Michael Alfaro <u201618332@upc.edu.pe>
2022-05-02 20:13:05 -0500

    Create CalificacionImpl.java

---
+++ Sonic/src/main/java/pe/edu/upc/daoimpls/CalificacionImpl.java
@@ -0,0 +1,43 @@
+package pe.edu.upc.daoimpls;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.transaction.Transactional;
+
+import pe.edu.upc.daointerfaces.ICalificacionDao;
+import pe.edu.upc.entities.Calificacion;
+
+public class CalificacionImpl implements ICalificacionDao {
+
+	@PersistenceContext(unitName = "Sonic")
+	private EntityManager em;
+
+	@Transactional
+	@Override
+	public void insert(Calificacion ca) {
+		try {
+			em.persist(ca);
+		} catch (Exception ca2) {
+			System.out.println("Error al insertar Calificacion en el DAO");
+		}
+
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Calificacion> list() {
+		List<Calificacion> listacalificacion = new ArrayList<Calificacion>();
+		try {
+			Query jpql = em.createQuery("from Calificacion ca");
+			listacalificacion = (List<Calificacion>) jpql.getResultList();
+		} catch (Exception e) {
+			System.out.println("Error al listar Calificacion en el DAO");
+		}
+		return listacalificacion;
+	}
+
+}
